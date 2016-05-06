56438ec8a90a8e662cb197ce71e218b1d3364960
Adam Wadsworth <siberware@gmail.com>
2016-05-06 06:02:11 -0700

    More todo stuff

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = 3000;
-var todos = [
-	{
-		id: 1,
-		description: 'Meet mom for lunch',
-		completed: false
-	},
-	{
-		id: 2,
-		description: 'Go to market',
-		completed: false
-	},
-	{
-		id: 3,
-		description: 'Fry chicken',
-		completed: true
-	}
-];
+var todoNextId = 1;
+var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -31,13 +21,14 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
-	var matched;
+	var matched = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function(todo) {
-		if(todoId === todo.id){
-			matched = todo;
-		}
-	});
+	// var matched;
+	// todos.forEach(function(todo) {
+	// 	if(todoId === todo.id){
+	// 		matched = todo;
+	// 	}
+	// });
 
 	if(matched){
 		res.json(matched)
@@ -45,6 +36,24 @@ app.get('/todos/:id', function(req, res) {
 	else{
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function(req, res) {
+	_.pick();
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) 
+		|| !_.isString(body.description) 
+		|| body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
+	body.id = todoNextId++;
+	todos.push(body)
+
+	res.json(body);
 });
 
 app.listen(PORT, function () {
